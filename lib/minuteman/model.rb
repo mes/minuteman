994be794c1303076bb937296dad30ca07b8f9fd0
Christian Becker <c.becker@mediaevent.services>
2017-10-27 20:28:27 +0200

    Removed Model.find as it was still using OHM semantics

--- lib/minuteman/model.rb
+++ lib/minuteman/model.rb
@@ -8,21 +8,8 @@ module Minuteman
       @type = args.first[:type]
     end
 
-    def self.find(*args)
-      looked_up = "#{self.name}::#{args.first[:type]}:#{args.first[:time]}:id"
-      potential_id = Minuteman.config.redis.call("GET", looked_up)
-
-      return nil if !potential_id
-
-      event = self[potential_id]
-      event.type = args.first[:type]
-      event.time = args.first[:time]
-
-      event
-    end
-
     def self.find_or_create(*args)
-      find(*args) || create(*args)
+      create(*args)
     end
 
     def self.create(*args)
